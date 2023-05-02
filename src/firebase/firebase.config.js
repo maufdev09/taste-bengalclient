bb89ded3b4e22bf0364f5155273a50335eac2a34
maufdev09 <marufdev09@gmail.com>
2023-05-02 10:57:27 +0600

    first commit

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyCUg8gKBr3UDvNyUklWKoX3xBezUWi3cIs",
+  authDomain: "taste-of-bengal-ef974.firebaseapp.com",
+  projectId: "taste-of-bengal-ef974",
+  storageBucket: "taste-of-bengal-ef974.appspot.com",
+  messagingSenderId: "782972542507",
+  appId: "1:782972542507:web:e5afa260635a6ba620d46e",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export default app;
